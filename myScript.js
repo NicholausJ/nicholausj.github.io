e40518a81eec6d4b5160cfc01b27bb1ce5348d86
Nick J <nicholausj@gmail.com>
2018-10-14 13:51:00 -1000

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,5 +1,5 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function myFunction1() {
   var x = document.getElementById("my-topnav");
   if (x.className === "topnav") {
     x.className += " responsive";
